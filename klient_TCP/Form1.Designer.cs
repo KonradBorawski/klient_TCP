e5ab65dcd091c0671e727c5b0d74178858a6a789
student <student@PC-214>
2017-05-25 15:04:53 +0200

    Interfejs i klient TCP zakonczony

--- klient_TCP/Form1.Designer.cs
+++ klient_TCP/Form1.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Info_o_polaczeniu = new System.Windows.Forms.ListBox();
+            this.Polacz = new System.Windows.Forms.Button();
+            this.Port = new System.Windows.Forms.Label();
+            this.Adres = new System.Windows.Forms.Label();
+            this.myPort = new System.Windows.Forms.NumericUpDown();
+            this.myAdress = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Info_o_polaczeniu
+            // 
+            this.Info_o_polaczeniu.FormattingEnabled = true;
+            this.Info_o_polaczeniu.Location = new System.Drawing.Point(30, 76);
+            this.Info_o_polaczeniu.Name = "Info_o_polaczeniu";
+            this.Info_o_polaczeniu.Size = new System.Drawing.Size(329, 199);
+            this.Info_o_polaczeniu.TabIndex = 0;
+            // 
+            // Polacz
+            // 
+            this.Polacz.Location = new System.Drawing.Point(165, 300);
+            this.Polacz.Name = "Polacz";
+            this.Polacz.Size = new System.Drawing.Size(75, 23);
+            this.Polacz.TabIndex = 3;
+            this.Polacz.Text = "Połącz";
+            this.Polacz.UseVisualStyleBackColor = true;
+            this.Polacz.Click += new System.EventHandler(this.Polacz_Click);
+            // 
+            // Port
+            // 
+            this.Port.AutoSize = true;
+            this.Port.Location = new System.Drawing.Point(273, 9);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(26, 13);
+            this.Port.TabIndex = 4;
+            this.Port.Text = "Port";
+            // 
+            // Adres
+            // 
+            this.Adres.AutoSize = true;
+            this.Adres.Location = new System.Drawing.Point(85, 9);
+            this.Adres.Name = "Adres";
+            this.Adres.Size = new System.Drawing.Size(37, 13);
+            this.Adres.TabIndex = 5;
+            this.Adres.Text = "Adres ";
+            // 
+            // myPort
+            // 
+            this.myPort.Location = new System.Drawing.Point(225, 31);
+            this.myPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.myPort.Name = "myPort";
+            this.myPort.Size = new System.Drawing.Size(120, 20);
+            this.myPort.TabIndex = 7;
+            // 
+            // myAdress
+            // 
+            this.myAdress.Location = new System.Drawing.Point(43, 31);
+            this.myAdress.Name = "myAdress";
+            this.myAdress.Size = new System.Drawing.Size(120, 20);
+            this.myAdress.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 349);
+            this.Controls.Add(this.myAdress);
+            this.Controls.Add(this.myPort);
+            this.Controls.Add(this.Adres);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.Polacz);
+            this.Controls.Add(this.Info_o_polaczeniu);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.myPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox Info_o_polaczeniu;
+        private System.Windows.Forms.Button Polacz;
+        private System.Windows.Forms.Label Port;
+        private System.Windows.Forms.Label Adres;
+        private System.Windows.Forms.NumericUpDown myPort;
+        private System.Windows.Forms.TextBox myAdress;
     }
 }
 
--- klient_TCP/Form1.cs
+++ klient_TCP/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace klient_TCP
 {
@@ -15,6 +16,24 @@ namespace klient_TCP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Polacz_Click(object sender, EventArgs e)
+        {
+            string host = myAdress.Text;
+            int port = System.Convert.ToInt32(myPort.Value);
+
+            try
+            {
+                TcpClient klient = new TcpClient(host, port);
+                Info_o_polaczeniu.Items.Add("Nawiązano połączenie z" + host + "na porcie" + port);
+                klient.Close();
+            }
+            catch(Exception ex)
+            {
+                Info_o_polaczeniu.Items.Add("Błąd: Nie udało się nawiązać połączenia!");
+                MessageBox.Show(ex.ToString(), "Błąd");
+            }
         }
     }
 }
